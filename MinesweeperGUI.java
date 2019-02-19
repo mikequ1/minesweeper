aa98e0c50dbbcc49fb55a4fef4a8d4b47a7b2e90
BigWillyMan420 <42596169+BigWillyMan420@users.noreply.github.com>
2019-02-19 08:53:31 -0800

    optimization

--- MinesweeperGUI.java
+++ MinesweeperGUI.java
@@ -9,7 +9,7 @@ import javax.swing.BorderFactory;
 public class MinesweeperGUI extends JFrame {
     //field variables
     private static GraphicsConfiguration gc;
-    private JPanel panels[];
+    private JLabel labels[];
     private Toolkit toolkit;
     private MediaTracker tracker;
     //====================================================================================================================================================
@@ -21,15 +21,17 @@ public class MinesweeperGUI extends JFrame {
         tracker = new MediaTracker(this);
         Image image = toolkit.getImage("bombflagged.gif");
         tracker.addImage(image, 0);
-        panels = new JPanel[Minesweeper.getWidth() * Minesweeper.getHeight()];
+        labels = new JLabel[Minesweeper.getWidth() * Minesweeper.getHeight()];
         Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
         for (int k=0; k<Minesweeper.getWidth() * Minesweeper.getHeight(); k++) {
-            panels[k] = new JPanel();
-            panels[k].setPreferredSize(new Dimension(10,10));
-            frame.add(panels[k]);
-            panels[k].setBorder(border);
-            
-            panels[k].add(new JLabel(spaces[k].toString()));
+            labels[k] = new JLabel(spaces[k].toString());
+            labels[k].setPreferredSize(new Dimension(10,10));
+            frame.add(labels[k]);
+            labels[k].setBorder(border);
+
+            // Set the label's font size to the newly determined size.
+            labels[k].setFont(new Font("Sans-Serif", Font.PLAIN, 25));
+            labels[k].setHorizontalAlignment(SwingConstants.CENTER);
         }
         frame.setLayout(new GridLayout(Minesweeper.getHeight(),Minesweeper.getWidth()));
         frame.setTitle("Minesweeper");
