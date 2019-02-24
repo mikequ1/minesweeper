d1546ea7e2d9945c144f58995df92af8395114fb
BigWillyMan420 <42596169+BigWillyMan420@users.noreply.github.com>
2019-02-24 15:36:41 -0800

    initial clear + open

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,136 +1,134 @@
-/**
- * Minesweeper
- * @author Diego Brown
- * @version
- */
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.*;
+import java.awt.event.*;
 
-//imports
-import java.util.Random;
-//====================================================================================================================================================
+public class Minesweeper extends JFrame implements ActionListener {
+    private int width = 0;
+    private int height = 0;
+    private int mines = 0;
 
-//classes
-public class Minesweeper {
-    //field variables
-    private static int width, height, mines;
-    private static int spaceID = 0;
-    public Space spaces[];
-    private MinesweeperGUI main;
-    //====================================================================================================================================================
+    private Toolkit toolkit;
+    private MediaTracker tracker;
+    private JFrame frame = new JFrame();
 
-    //constructors
-    public Minesweeper (int width, int height, int mines) {
-        resetSpaceID();
-        this.width = width;
-        this.height = height;
-        this.mines = mines;
-        spaces = new Space[width * height];
-        createGrid();
-        assignGrid();
-        assignNeighbors();
-        main = new MinesweeperGUI(spaces);
-    }
-    //====================================================================================================================================================
-
-    //private methods
-    private void createGrid() {
-        System.out.println("==================================================");
-        for (int y=1; y<height+1; y++) {
-            for (int x=1; x<width+1; x++) {
-                spaces[spaceID] = new Space(x, y, spaceID);
-                spaceID++;
-            }
-        }
-        System.out.println("==================================================");
-    }
-
-    private void assignGrid() {
-        Random rand = new Random();        
-        do {
-            int index = rand.nextInt(width * height);
-            if (spaces[index].isMine() == false) {
-                spaces[index].makeMine();
-                mines--;
-            }
-        } while (mines > 0);
-        System.out.println("==================================================");
+    JTextField widthField = new JTextField("Width",2);
+    JTextField heightField = new JTextField("Height",2);
+    JTextField mineField = new JTextField("Mines",3);
+    
+    Color green = new Color(0,100,0);
+    Color orange = new Color(255,120,0);
+    
+    Minesweeper minesweeper;
+    
+    public Minesweeper() {
+        drawMenu();
     }
     
-    private int getNeighbors (Space space) {
-        int adjacent = 0;
-        int currentIndex = space.getSpaceID();        
-        //North
-        if (currentIndex - width >= 0 && spaces[currentIndex-width].isMine() == true) {
-            adjacent++;
-        }
-        //South
-        if (currentIndex + width <= width * height - 1 && spaces[currentIndex+width].isMine() == true) {
-            adjacent++;
-        }
-        //West
-        if (currentIndex - 1 >= 0 && space.getYPos() == spaces[currentIndex-1].getYPos() && 
-            spaces[currentIndex-1].isMine() == true) {
-            adjacent++;
-        }
-        //East
-        if (currentIndex + 1 <= width * height - 1 && space.getYPos() == spaces[currentIndex+1].getYPos() &&
-            spaces[currentIndex+1].isMine() == true) {
-            adjacent++;
-        }
-        //North-West
-        if (currentIndex - width - 1 >= 0 && space.getYPos() - 1 == spaces[currentIndex-width-1].getYPos() &&
-            spaces[currentIndex-width-1].isMine() == true) {
-            adjacent++;
-        }
-        //North-East
-        if (currentIndex - width + 1 >= 0 && currentIndex - width + 1 <= width * height - 1 &&
-            space.getYPos() - 1 == spaces[currentIndex-width+1].getYPos() && spaces[currentIndex-width+1].isMine() == true) {
-            adjacent++;
-        }
-        //South-West
-        if (currentIndex + width - 1 >= 0 && currentIndex + width - 1 <= width * height - 1 &&
-            space.getYPos() + 1 == spaces[currentIndex+width-1].getYPos() && spaces[currentIndex+width-1].isMine() == true) {
-            adjacent++;
-        }
-        //South-East
-        if (currentIndex + width + 1 <= width * height - 1 && space.getYPos() + 1 == spaces[currentIndex+width+1].getYPos() &&
-            spaces[currentIndex+width+1].isMine() == true) {
-            adjacent++;
-        }
-        return adjacent;
-    }
-    
-    private void assignNeighbors() {
-        for (int i=0; i<width*height; i++) {
-            spaces[i].setAdjacentMines(getNeighbors(spaces[i]));
-        }
-        System.out.println("==================================================");
-    }
-    //====================================================================================================================================================
+    public void drawMenu() {
+        //setting the icon
+        toolkit = Toolkit.getDefaultToolkit();
+        tracker = new MediaTracker(this);
+        Image image = toolkit.getImage("bombflagged.gif");
+        tracker.addImage(image, 0);
 
-    //mutator methods
-    private void resetSpaceID() {
-        spaceID = 0;
-    }
-    //====================================================================================================================================================
-    
-    //accessor methods
-    public Space getSpaceBySpaceID (int spaceID) {
-        return spaces[spaceID];
-    }
-    
-    public static int getWidth() {
-        return width;
-    }
-    
-    public static int getHeight() {
-        return height;
-    }
-    //====================================================================================================================================================
+        JFrame.setDefaultLookAndFeelDecorated(true);        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setIconImage(image);
 
-    //main method
+        JPanel panel = new JPanel();
+        BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+        panel.setLayout(boxlayout);
+        panel.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
+
+        //beginner button
+        JButton beginner = new JButton("Beginner");
+        beginner.setMnemonic(KeyEvent.VK_E);
+        beginner.setActionCommand("beginner");
+        beginner.setToolTipText("9x9, 10 mines.");
+        beginner.setOpaque(true);
+        beginner.setForeground(green);
+        beginner.addActionListener(this);
+
+        //intermediate button
+        JButton intermediate = new JButton("Intermediate");
+        intermediate.setMnemonic(KeyEvent.VK_E);
+        intermediate.setActionCommand("intermediate");
+        intermediate.setToolTipText("16x16, 40 mines.");
+        intermediate.setOpaque(true);
+        intermediate.setForeground(orange);
+        intermediate.addActionListener(this);
+
+        //expert button
+        JButton expert = new JButton("Expert");
+        expert.setMnemonic(KeyEvent.VK_E);
+        expert.setActionCommand("expert");
+        expert.setToolTipText("30x16, 99 mines.");
+        expert.setOpaque(true);
+        expert.setForeground(Color.RED);
+        expert.addActionListener(this);
+
+        //text fields
+        widthField.setToolTipText("Enter a width.");
+        widthField.setActionCommand("widthSet");
+        widthField.addActionListener(this);
+
+        heightField.setToolTipText("Enter a height.");
+        heightField.setActionCommand("heightSet");
+        heightField.addActionListener(this);
+
+        mineField.setToolTipText("Enter the number of mines.");
+        mineField.setActionCommand("minesSet");
+        mineField.addActionListener(this);
+
+        //custom button
+        JButton custom = new JButton("Submit");
+        custom.setMnemonic(KeyEvent.VK_E);
+        custom.setActionCommand("submit");
+        custom.setToolTipText("Starts a game with custom width, height, and mines.");
+        custom.addActionListener(this);
+
+        //Labels
+        JLabel presets = new JLabel("Presets");
+        JLabel customs = new JLabel("Custom");
+
+        panel.add(presets);
+        panel.add(beginner); 
+        panel.add(intermediate);
+        panel.add(expert);
+        panel.add(customs);
+        panel.add(widthField);
+        panel.add(heightField);
+        panel.add(mineField);
+        panel.add(custom);
+        frame.add(panel);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if ("beginner".equals(e.getActionCommand())) {
+            GameLogic game = new GameLogic(9,9,10);
+        }
+        else if ("intermediate".equals(e.getActionCommand())) {
+            GameLogic game = new GameLogic(16,16,40);
+        }
+        else if ("expert".equals(e.getActionCommand())) {
+            GameLogic game = new GameLogic(30,16,99);
+        }
+        else if ("submit".equals(e.getActionCommand())) {
+            GameLogic game = new GameLogic(Integer.parseInt(widthField.getText()),
+                Integer.parseInt(heightField.getText()),Integer.parseInt(mineField.getText()));
+        }
+        frame.setVisible(false);
+        frame.dispose();
+    }
+
     public static void main (String[] args) {
-
+        Minesweeper minesweeper = new Minesweeper();
+        //minesweeper.drawMenu();
     }
-    //====================================================================================================================================================
 }
-//====================================================================================================================================================
--- MinesweeperGUI.java
+++ MinesweeperGUI.java
@@ -1,47 +1,257 @@
-//imports
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.border.Border;
+import javax.swing.border.*;
 import javax.swing.BorderFactory;
-//====================================================================================================================================================
+import java.awt.event.*;
 
-//classes
-public class MinesweeperGUI extends JFrame {
-    //field variables
+public class MinesweeperGUI extends JFrame implements ActionListener {
     private static GraphicsConfiguration gc;
-    private JLabel labels[];
-    private Toolkit toolkit;
-    private MediaTracker tracker;
-    //====================================================================================================================================================
+    private JButton buttons[];
+    private Space spaces[];
+    private Toolkit toolkit = Toolkit.getDefaultToolkit();
+    private MediaTracker tracker = new MediaTracker(this);
+    JFrame frame = new JFrame(gc);
+    JFrame win;
+    JPanel winpanel;
+    private GameLogic game;
+    Minesweeper newgame;
+    Image image = toolkit.getImage("bombflagged.gif");
+    Color brown = new Color(150,75,0);
+    Color navyblue = new Color(0,0,140);
+    Color cyan = new Color(0,128,128);
+    Color green = new Color(0,100,0);
 
-    //constructors
-    MinesweeperGUI(Space spaces[]) {
-        JFrame frame = new JFrame(gc);
-        toolkit = Toolkit.getDefaultToolkit();
-        tracker = new MediaTracker(this);
-        Image image = toolkit.getImage("bombflagged.gif");
+    public MinesweeperGUI(Space spaces[], GameLogic game) {
+        this.spaces = spaces;
+        this.game = game;
         tracker.addImage(image, 0);
-        labels = new JLabel[Minesweeper.getWidth() * Minesweeper.getHeight()];
-        Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
-        for (int k=0; k<Minesweeper.getWidth() * Minesweeper.getHeight(); k++) {
-            labels[k] = new JLabel(spaces[k].toString());
-            labels[k].setPreferredSize(new Dimension(10,10));
-            frame.add(labels[k]);
-            labels[k].setBorder(border);
 
+        buttons = new JButton[game.getWidth() * game.getHeight()];
+
+        Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+
+        for (int k=0; k<game.getWidth() * game.getHeight(); k++) {
+            buttons[k] = new JButton();
+            buttons[k].setPreferredSize(new Dimension(10,10));
+            frame.add(buttons[k]);
+            buttons[k].setBorder(border);
             // Set the label's font size to the newly determined size.
-            labels[k].setFont(new Font("Sans-Serif", Font.PLAIN, 25));
-            labels[k].setHorizontalAlignment(SwingConstants.CENTER);
+            buttons[k].setFont(new Font("Sans-Serif", Font.PLAIN, 25));
+            buttons[k].setHorizontalAlignment(SwingConstants.CENTER);
+            //buttons[k].setMnemonic(KeyEvent.VK_E);
+            buttons[k].setActionCommand(spaces[k].getSpaceIDString());
+            buttons[k].addActionListener(this);
+            buttons[k].setBackground(Color.GRAY);
+            //buttons[k].addMouseListener(new MouseAdapter());
+            buttons[k].setOpaque(true);
         }
-        frame.setLayout(new GridLayout(Minesweeper.getHeight(),Minesweeper.getWidth()));
+        frame.setLayout(new GridLayout(game.getHeight(),game.getWidth()));
         frame.setTitle("Minesweeper");
         frame.setIconImage(image);
         frame.setVisible(true);
-        frame.setSize(40 * Minesweeper.getWidth(), 40 * Minesweeper.getHeight());
+        frame.setSize(40 * game.getWidth(), 40 * game.getHeight());
         frame.setLocation(200, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
     }
-    //====================================================================================================================================================
+
+    public void open(JButton button, Space space) {
+        int id = space.getSpaceID();
+        int width = game.getWidth();
+        int height = game.getHeight();
+        int total = width * height - 1;
+        button.setBackground(Color.WHITE);
+        space.uncover();
+
+        if (space.isMine() && !space.isFlagged()) {
+            win = new JFrame(gc);
+            //button.setText("\uD83D\uDCA3");
+            for (int i=0; i<buttons.length; i++) {
+                if (spaces[i].isMine()) {
+                    buttons[i].setText("*");
+                    buttons[i].setBackground(Color.RED);
+                }
+            }
+            win.setTitle("You lose!");
+            tracker.addImage(image, 0);
+
+            win.setDefaultLookAndFeelDecorated(true);        
+            win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            win.setResizable(false);
+            win.setIconImage(image);
+
+            winpanel = new JPanel();
+            BoxLayout boxlayout = new BoxLayout(winpanel, BoxLayout.Y_AXIS);
+            winpanel.setLayout(boxlayout);
+            winpanel.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
+
+            JButton playagain = new JButton("Play again?");
+            playagain.setMnemonic(KeyEvent.VK_E);
+            playagain.setActionCommand("playagain");
+            playagain.setToolTipText("Restarts the game");
+            playagain.addActionListener(this);
+
+            JButton back = new JButton("Change difficulty");
+            back.setMnemonic(KeyEvent.VK_E);
+            back.setActionCommand("return");
+            back.setToolTipText("Returns to the difficulty screen");
+            back.addActionListener(this);
+
+            winpanel.add(playagain);
+            winpanel.add(back);
+            win.add(winpanel);
+            win.pack();
+            win.setVisible(true);
+            game.hasWon = true;
+        }
+        else if (space.isMine() && space.isFlagged()) {
+
+        }
+        else if (!space.isMine()) {
+            if (space.getAdjacentMines() == 0)
+                button.setText("");
+            else {
+                if (Integer.parseInt(space.getAdjacentMinesString()) == 1) {
+                    button.setForeground(Color.BLUE);
+                }
+                else if (Integer.parseInt(space.getAdjacentMinesString()) == 2) {
+                    button.setForeground(green);
+                }
+                else if (Integer.parseInt(space.getAdjacentMinesString()) == 3) {
+                    button.setForeground(Color.RED);
+                }
+                else if (Integer.parseInt(space.getAdjacentMinesString()) == 4) {
+                    button.setForeground(navyblue);
+                }
+                else if (Integer.parseInt(space.getAdjacentMinesString()) == 5) {
+                    button.setForeground(brown);
+                }
+                else if (Integer.parseInt(space.getAdjacentMinesString()) == 6) {
+                    button.setForeground(cyan);
+                }
+                else if (Integer.parseInt(space.getAdjacentMinesString()) == 7) {
+                    button.setForeground(Color.BLACK);
+                }
+                else if (Integer.parseInt(space.getAdjacentMinesString()) == 8) {
+                    button.setForeground(Color.GRAY);
+                }
+                button.setText(space.getAdjacentMinesString());
+            }
+            //North
+            if (id - width >= 0 && spaces[id-width].isMine() == false && spaces[id-width].getCovered() == true && space.getAdjacentMines() == 0){
+                open(buttons[id-width],spaces[id-width]);
+            }
+            //South  
+            if (id + width <= total && spaces[id+width].isMine() == false && spaces[id+width].getCovered() == true && space.getAdjacentMines() == 0)
+            {
+                open(buttons[id+width],spaces[id+width]);
+            }
+            //West
+            if (id - 1 >= 0 && space.getYPos() == spaces[id-1].getYPos() && spaces[id-1].isMine() == false
+            && spaces[id-1].getCovered() == true && space.getAdjacentMines() == 0) {
+                open(buttons[id-1],spaces[id-1]);
+            }
+            //East
+            if (id + 1 <= total && space.getYPos() == spaces[id+1].getYPos() &&
+            spaces[id+1].isMine() == false && spaces[id+1].getCovered() == true && space.getAdjacentMines() == 0) {
+                open(buttons[id+1],spaces[id+1]);
+            }
+            //North-West
+            if (id - width - 1 >= 0 && space.getYPos() - 1 == spaces[id-width-1].getYPos() &&
+            spaces[id-width-1].isMine() == false && spaces[id-width-1].getCovered() == true &&
+            space.getAdjacentMines() == 0) {
+                open(buttons[id-width-1],spaces[id-width-1]);
+            }
+            //North-East
+            if (id - width + 1 >= 0 && id - width + 1 <= width * height - 1 &&
+            space.getYPos() - 1 == spaces[id-width+1].getYPos() && spaces[id-width+1].isMine() == false &&
+            spaces[id-width+1].getCovered() == true &&  space.getAdjacentMines() == 0) {
+                open(buttons[id-width+1],spaces[id-width+1]);
+            }
+            //South-West
+            if (id + width - 1 >= 0 && id + width - 1 <= width * height - 1 &&
+            space.getYPos() + 1 == spaces[id+width-1].getYPos() && spaces[id+width-1].isMine() == false
+            && spaces[id+width-1].getCovered() == true &&  space.getAdjacentMines() == 0) {
+                open(buttons[id+width-1],spaces[id+width-1]);
+            }
+            //South-East
+            if (id + width + 1 <= width * height - 1 && space.getYPos() + 1 == spaces[id+width+1].getYPos() &&
+            spaces[id+width+1].isMine() == false && spaces[id+width+1].getCovered() == true &&  space.getAdjacentMines() == 0) {
+                open(buttons[id+width+1],spaces[id+width+1]);
+            }
+        }
+        winGame();
+    }
+
+    public void winGame()
+    {   
+        int coveredCount = 0;
+        for (int i = 0; i < spaces.length; i++)
+        {
+            if (spaces[i].getCovered() == true)
+                coveredCount++;   
+        }
+        if (coveredCount == game.getMines() && game.hasWon == false)
+        {   
+            win = new JFrame(gc);
+            win.setTitle("You win!");
+            tracker.addImage(image, 0);
+
+            win.setDefaultLookAndFeelDecorated(true);        
+            win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            win.setResizable(false);
+            win.setIconImage(image);
+
+            winpanel = new JPanel();
+            BoxLayout boxlayout = new BoxLayout(winpanel, BoxLayout.Y_AXIS);
+            winpanel.setLayout(boxlayout);
+            winpanel.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
+
+            JButton playagain = new JButton("Play again?");
+            playagain.setMnemonic(KeyEvent.VK_E);
+            playagain.setActionCommand("playagain");
+            playagain.setToolTipText("Restarts the game");
+            playagain.addActionListener(this);
+
+            JButton back = new JButton("Change difficulty");
+            back.setMnemonic(KeyEvent.VK_E);
+            back.setActionCommand("return");
+            back.setToolTipText("Returns to the difficulty screen");
+            back.addActionListener(this);
+
+            winpanel.add(playagain);
+            winpanel.add(back);
+            win.add(winpanel);
+            win.pack();
+            win.setVisible(true);
+            game.hasWon = true;
+            //System.out.println("You win");
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if ("playagain".equals(e.getActionCommand())) {
+            game = new GameLogic(game.getWidth(),game.getHeight(),game.getMines());
+            frame.setVisible(false);
+            frame.dispose();
+            win.setVisible(false);
+            win.dispose();
+        }
+        else if ("return".equals(e.getActionCommand())) {
+            newgame = new Minesweeper();
+            frame.setVisible(false);
+            frame.dispose();
+            win.setVisible(false);
+            win.dispose();
+        }
+        else if (game.firstClick) {
+            game.assignGrid(Integer.parseInt(e.getActionCommand()));
+            game.assignNeighbors();
+            open(buttons[Integer.parseInt(e.getActionCommand())],spaces[Integer.parseInt(e.getActionCommand())]);
+            game.firstClick = false;
+        }
+        else {
+            open(buttons[Integer.parseInt(e.getActionCommand())],spaces[Integer.parseInt(e.getActionCommand())]);
+        }
+    }
 }
-//====================================================================================================================================================
